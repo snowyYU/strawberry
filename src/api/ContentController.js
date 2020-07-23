97b871861ec698da37ccd0af2a166f1d092a68ee
snowyyu <731444920@qq.com>
2020-07-23 18:40:06 +0800

    新增

---
+++ src/api/ContentController.js
@@ -0,0 +1,47 @@
+import Post from '../model/Post'
+class ContentController {
+  async getPostList(ctx) {
+    // const post = new Post({
+    //   title: 'test title6',
+    //   content: 'test content6',
+    //   catalog: 'advise',
+    //   fav: 20,
+    //   isEnd: '0',
+    //   reads: '0',
+    //   answer: '0',
+    //   status: '0',
+    //   isTop: '0',
+    //   sort: '0',
+    //   tags: [{ name: '精华' }],
+    // })
+    // const tmp = await post.save()
+    // console.log('tcl', tmp)
+    const body = ctx.query
+    const sort = body.sort ? body.sort : 'created'
+    const page = body.page ? parseInt(body.page) : 0
+    const limit = body.limit ? parseInt(body.limit) : 20
+    const options = {}
+    if (typeof body.catalog !== 'undefined' && body.catalog !== '') {
+      options.catalog = body.catalog
+    }
+    if (typeof body.isTop !== 'undefined') {
+      options.isTop = body.isTop
+    }
+    if (typeof body.status !== 'undefined') {
+      options.status = body.status
+    }
+    if (typeof body.isEnd !== 'undefined') {
+      options.isEnd = body.isEnd
+    }
+    if (typeof body.tag !== 'undefined' && body.tag !== '') {
+      options.tags = { $elemMatch: { name: body.tag } }
+    }
+    const result = await Post.getList(options, sort, page, limit)
+    ctx.body = {
+      code: 200,
+      data: result,
+      msg: '获取列表成功',
+    }
+  }
+}
+export default new ContentController()
