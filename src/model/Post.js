1789d78d26473e323e1b1b5baa3faf5b84a87064
snowyyu <731444920@qq.com>
2020-07-23 18:39:34 +0800

    回退 webpack merge 到 4 版本，新增文章列表相关的功能

--- src/model/Post.js
+++ src/model/Post.js
@@ -1,23 +1,48 @@
-import mongoose from "../config/DBHelpler";
+import mongoose from '../config/DBHelpler'
+import moment from 'moment'
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const PostSchema = new Schema({
-  uid: { type: String, ref: "users" },
+  uid: { type: String, ref: 'users' },
   title: { type: String },
   content: { type: String },
   created: { type: Date },
   catalog: { type: String },
   fav: { type: String },
-  isEnd: { type: String, default: "0" },
+  isEnd: { type: String, default: '0' },
   reads: { type: Number, default: 0 },
   answer: { type: Number, default: 0 },
-  status: { type: String, default: "0" },
-  isTop: { type: String, default: "0" },
+  status: { type: String, default: '0' },
+  isTop: { type: String, default: '0' },
   sort: { type: String, default: 100 },
-  tags: { type: String },
-});
+  tags: { type: Array },
+})
 
-const PostModel = mongoose.model("users", PostSchema);
+PostSchema.pre('save', function (next) {
+  this.created = moment().format('YYYY-MM-DD HH:mm:ss')
+  next()
+})
 
-export default PostModel;
+PostSchema.statics = {
+  /**
+   * 获取文章列表数据
+   * @param {Object} options
+   * @param {String} sort
+   * @param {Number} page
+   * @param {Number} limit
+   */
+  getList(options, sort, page, limit) {
+    return this.find(options)
+      .sort({ [sort]: -1 })
+      .skip(page * limit)
+      .limit(limit)
+      .populate({
+        path: 'uid',
+        select: 'name',
+      })
+  },
+}
+const PostModel = mongoose.model('posts', PostSchema)
+
+export default PostModel
